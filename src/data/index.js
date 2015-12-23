2fa119fe0aae47818b87c1cf705c756d3d387318
Sascha Depold <sascha@depold.com>
2015-12-23 22:22:04 +0100

    Enable post import

--- src/data/index.js
+++ src/data/index.js
@@ -8,38 +8,8 @@ export default function importData (space, contentTypes, data) {
   return space.getEntries().then((entries) => {
     return Promise.all([
       Author.importData(entries, space, contentTypes.Author, data),
-      // Post.importData(entries, space, contentTypes.Post, data),
+      Post.importData(entries, space, contentTypes.Post, data),
       Tag.importData(entries, space, contentTypes.Tag, data)
-      // importTags(entries, space, contentTypes, data),
-      // importPosts(entries, space, contentTypes, data)
     ]);
   });
 }
-//
-// function importUsers (entries, space, contentTypes, data) {
-//   return validateUsersData
-//
-//   // return importEntities(...arguments, 'user', (user) => {
-//   //   return {
-//   //     sys: { id: user.slug },
-//   //     fields: {
-//   //       name: { 'en-US': user.name },
-//   //       slug: { 'en-US': user.slug },
-//   //       email: { 'en-US': user.email },
-//   //       image: { 'en-US': user.image }
-//   //     }
-//   //   };
-//   // });
-// }
-//
-// function importTags (entries, space, contentTypes, data) {
-//   return importEntities(...arguments, 'tag', (tag) => {
-//     return {
-//       sys: { id: tag.slug },
-//       fields: {
-//         name: { 'en-US': tag.name },
-//         slug: { 'en-US': tag.slug }
-//       }
-//     };
-//   });
-// }
--- test/data/index.test.js
+++ test/data/index.test.js
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import { prepareApiStub, fakeContentTypes } from '../support/helper';
 import importData from '../../src/data';
 import * as Author from '../../src/data/author';
-// import * as Post from '../../src/data/post';
+import * as Post from '../../src/data/post';
 import * as Tag from '../../src/data/tag';
 import * as sinon from 'sinon';
 
@@ -23,14 +23,16 @@ describe('data', () => {
 
     it('imports data', () => {
       let authorMock = sinon.mock(Author);
+      let postMock = sinon.mock(Post);
       let tagMock = sinon.mock(Tag);
 
       authorMock.expects('importData').withArgs([], space, contentTypes.Author, data);
-      // mocks.push(sinon.mock(Post).expects('importData').withArgs([], space, contentTypes.Post, data));
+      postMock.expects('importData').withArgs([], space, contentTypes.Post, data);
       tagMock.expects('importData').withArgs([], space, contentTypes.Tag, data);
 
       return importData(space, contentTypes, data).then(() => {
         authorMock.restore();
+        postMock.restore();
         tagMock.restore();
       });
     });
@@ -45,15 +47,15 @@ describe('data', () => {
       });
     });
 
-    // it.only('rejects malformed posts', () => {
-    //   data.posts.push({ foo: 'bar' });
-    //
-    //   return importData(space, contentTypes, data).then(() => {
-    //     expect('should not end up here').to.eql(null);
-    //   }, (err) => {
-    //     expect(err.message).to.eql('Invalid post properties found: foo');
-    //   });
-    // });
+    it('rejects malformed posts', () => {
+      data.posts.push({ foo: 'bar' });
+
+      return importData(space, contentTypes, data).then(() => {
+        expect('should not end up here').to.eql(null);
+      }, (err) => {
+        expect(err.message).to.eql('Invalid post properties found: foo');
+      });
+    });
 
     it('rejects malformed tags', () => {
       data.tags.push({ name: 'General', slug: 'general', foo: 'bar' });
